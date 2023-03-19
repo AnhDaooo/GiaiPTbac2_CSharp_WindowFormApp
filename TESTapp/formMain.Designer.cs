9c1dc1015d23543a53540b086b698ab69b546324
Nanami <anhdao11a5cmb1920@gmail.com>
2023-03-19 20:36:17 +0700

    first commit

---
+++ TESTapp/formMain.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace TESTapp
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtHeSoA = new System.Windows.Forms.TextBox();
+            this.txtHeSoB = new System.Windows.Forms.TextBox();
+            this.txtHeSoC = new System.Windows.Forms.TextBox();
+            this.btGiaiPhuongTrinh = new System.Windows.Forms.Button();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtHeSoA
+            // 
+            this.txtHeSoA.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtHeSoA.BackColor = System.Drawing.SystemColors.Window;
+            this.txtHeSoA.Location = new System.Drawing.Point(302, 43);
+            this.txtHeSoA.Multiline = true;
+            this.txtHeSoA.Name = "txtHeSoA";
+            this.txtHeSoA.Size = new System.Drawing.Size(351, 36);
+            this.txtHeSoA.TabIndex = 1;
+            this.txtHeSoA.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtHeSoB
+            // 
+            this.txtHeSoB.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtHeSoB.Location = new System.Drawing.Point(302, 98);
+            this.txtHeSoB.Multiline = true;
+            this.txtHeSoB.Name = "txtHeSoB";
+            this.txtHeSoB.Size = new System.Drawing.Size(351, 33);
+            this.txtHeSoB.TabIndex = 2;
+            // 
+            // txtHeSoC
+            // 
+            this.txtHeSoC.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtHeSoC.Location = new System.Drawing.Point(302, 152);
+            this.txtHeSoC.Multiline = true;
+            this.txtHeSoC.Name = "txtHeSoC";
+            this.txtHeSoC.Size = new System.Drawing.Size(351, 34);
+            this.txtHeSoC.TabIndex = 3;
+            // 
+            // btGiaiPhuongTrinh
+            // 
+            this.btGiaiPhuongTrinh.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btGiaiPhuongTrinh.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btGiaiPhuongTrinh.Location = new System.Drawing.Point(379, 216);
+            this.btGiaiPhuongTrinh.Name = "btGiaiPhuongTrinh";
+            this.btGiaiPhuongTrinh.Size = new System.Drawing.Size(194, 30);
+            this.btGiaiPhuongTrinh.TabIndex = 4;
+            this.btGiaiPhuongTrinh.Text = "Giải Phương Trình";
+            this.btGiaiPhuongTrinh.UseVisualStyleBackColor = false;
+            this.btGiaiPhuongTrinh.Click += new System.EventHandler(this.btGPT_Click);
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(166, 314);
+            this.txtKetQua.Multiline = true;
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(640, 207);
+            this.txtKetQua.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(179, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 22);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Hệ số a:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(179, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Hệ số b:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(179, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 22);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Hệ số c:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.PowderBlue;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label4.Location = new System.Drawing.Point(163, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 22);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Kết quả:";
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(946, 549);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.btGiaiPhuongTrinh);
+            this.Controls.Add(this.txtHeSoC);
+            this.Controls.Add(this.txtHeSoB);
+            this.Controls.Add(this.txtHeSoA);
+            this.Name = "formMain";
+            this.Text = "Giải phương trình bậc 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtHeSoA;
+        private System.Windows.Forms.TextBox txtHeSoB;
+        private System.Windows.Forms.TextBox txtHeSoC;
+        private System.Windows.Forms.Button btGiaiPhuongTrinh;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TESTapp/formMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TESTapp
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btGPT_Click(object sender, EventArgs e)
+        {
+            float a, b, c, delta, x1, x2;
+
+            // lấy hệ số user đã nhập từ hộp
+            a = float.Parse(txtHeSoA.Text);
+            b = float.Parse(txtHeSoB.Text);
+            c = float.Parse(txtHeSoC.Text);
+
+            delta = b * b - 4 * a * c;
+
+            if(delta < 0)
+            {
+                txtKetQua.Text = "phương trình vô nghiệm";
+            }
+            else if(delta == 0)
+            {
+                x1 = -b / (2 * a);
+                txtKetQua.Text = "phương trình có nghiệm kép x = " + x1.ToString();
+            }
+            else
+            {
+                x1 = (float)(-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = (float)(-b - Math.Sqrt(delta)) / (2 * a);
+
+                txtKetQua.Text = "phương trình có 2 nghiệm phân biệt: " 
+                                    + "\r\n x1 = " + x1.ToString()
+                                    + "\r\n x2 = " + x2.ToString();
+            }
+        }
+    }
+}
